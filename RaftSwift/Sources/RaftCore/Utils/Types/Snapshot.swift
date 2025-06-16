ecba344b5f1040edd630beec1e31762bd49cc890
niklhut <49069026+niklhut@users.noreply.github.com>
2025-06-16 14:58:14 +0200

    feat(RaftSwift): add snapshot types

--- RaftSwift/Sources/RaftCore/Utils/Types/Snapshot.swift
+++ RaftSwift/Sources/RaftCore/Utils/Types/Snapshot.swift
@@ -1,11 +1,29 @@
 /// A snapshot of the entire state machine
-struct Snapshot {
+public struct Snapshot: Sendable, Codable {
     /// Index of last log entry included in the snapshot
-    var lastIncludedIndex: Int = 0
+    public var lastIncludedIndex: Int = 0
 
     /// Term of last log entry included in the snapshot
-    var lastIncludedTerm: Int = 0
+    public var lastIncludedTerm: Int = 0
 
     /// State machine state
-    var stateMachine: [String: String] = [:]
+    public var stateMachine: [String: String] = [:]
+
+    /// Initializes a new empty instance of the Snapshot struct.
+    public init() {}
+
+    /// Initializes a new instance of the Snapshot struct.
+    /// - Parameters:
+    ///   - lastIncludedIndex: The index of the last log entry included in the snapshot.
+    ///   - lastIncludedTerm: The term of the last log entry included in the snapshot.
+    ///   - stateMachine: The state machine state.
+    public init(
+        lastIncludedIndex: Int,
+        lastIncludedTerm: Int,
+        stateMachine: [String: String]
+    ) {
+        self.lastIncludedIndex = lastIncludedIndex
+        self.lastIncludedTerm = lastIncludedTerm
+        self.stateMachine = stateMachine
+    }
 }

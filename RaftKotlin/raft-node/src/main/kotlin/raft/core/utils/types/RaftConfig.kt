eceacd111374534ecc57fd8aaa542616ecb313a6
niklhut <49069026+niklhut@users.noreply.github.com>
2025-07-02 17:06:25 +0200

    fix(RaftKotlin): remove unused duplicate compaction threshold

--- RaftKotlin/raft-node/src/main/kotlin/raft/core/utils/types/RaftConfig.kt
+++ RaftKotlin/raft-node/src/main/kotlin/raft/core/utils/types/RaftConfig.kt
@@ -12,8 +12,4 @@ data class RaftConfig(
      * The interval of heartbeats in milliseconds
      */
     val heartbeatInterval: Int = 50,
-    /**
-     * The threshold of log entries to compact
-     */
-    val compactionThreshold: Int = 1000
 )

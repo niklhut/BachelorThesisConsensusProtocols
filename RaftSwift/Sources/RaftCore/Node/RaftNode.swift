8877ace74f46abbcff86f81d9b4ce31e95ecf17c
niklhut <49069026+niklhut@users.noreply.github.com>
2025-07-05 17:26:40 +0200

    fix(RaftSwift): fix version in diagnostics

--- RaftSwift/Sources/RaftCore/Node/RaftNode.swift
+++ RaftSwift/Sources/RaftCore/Node/RaftNode.swift
@@ -345,7 +345,7 @@ public actor RaftNode {
         DiagnosticsResponse(
             id: persistentState.ownPeer.id,
             implementation: "Swift",
-            version: "1",
+            version: "1.3.1",
             compactionThreshold: persistentState.config.compactionThreshold,
         )
     }
